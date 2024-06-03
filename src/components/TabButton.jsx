0d53c0a66b2ad073c9f4fb7b62757de81e188bb6
Nikilesh Raj N <nikilesh.raj-n@siemens-healthineers.com>
2024-06-03 18:02:06 +0530

    add all files

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  const handleClick = () => {
+    console.log("Hello World!", children);
+  }
+
   return (
     <list>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </list>
   );
 }
