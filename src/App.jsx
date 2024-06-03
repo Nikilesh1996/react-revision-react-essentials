8a5edae66a27fd748739dd9d8ed2624032acc9b6
Nikilesh Raj N <nikilesh.raj-n@siemens-healthineers.com>
2024-06-03 14:22:44 +0530

    restructured and added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-import reactImage from "./assets/react-core-concepts.png";
+
 import componentsImg from "./assets/components.png";
+import Header from "./components/Header";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({title, description, image}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import CoreConcept from "./components/CoreConcept";
 
 function App() {
   return (
