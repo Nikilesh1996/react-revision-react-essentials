b0a32677462fa74f2ffc7287d37225571f41ed9d
Nikilesh Raj N <nikilesh.raj-n@siemens-healthineers.com>
2024-06-03 18:15:09 +0530

    added handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+  const handleSelect = (selectedButton) => {
+    // selectedButton ["components", "jsx", "props", "state"]
+    console.log("Handling select from the app component", selectedButton);
+  };
+
   return (
     <div>
       <Header />
@@ -16,22 +21,23 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>            
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
-        <h2>Time to get started!</h2>
+        Dynamic content here
       </main>
     </div>
   );
